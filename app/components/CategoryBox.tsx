c402a969a988b8428c4c260689c913a2a8680afd
Harry <danghuuphuc001@gmail.com>
2023-09-04 15:20:02 +0700

    categories of accommodations and activities

---
+++ app/components/CategoryBox.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { IconType } from "react-icons";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+import qs from "query-string";
+import { categories } from "./navbar/Categories";
+
+interface CategoryBoxProps {
+  icon: IconType;
+  label: string;
+  selected: boolean;
+}
+
+const CategoryBox: React.FC<CategoryBoxProps> = ({
+  icon: Icon,
+  label,
+  selected,
+}) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    // look through params and parse it to string
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+      // console.log(currentQuery);
+    }
+
+    // spread currentQuery and add the new category
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+    // console.log(updatedQuery);
+
+    // check if a field is already selected and we remove from updatedQuery cause deselected
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    // generate string
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery, // pass the newest query
+      },
+      {
+        skipNull: true,
+      }
+    );
+
+    // console.log(url);
+
+    // push it to router
+    router.push(url);
+  }, [label, params, router]);
+
+  return (
+    <div
+      onClick={handleClick}
+      className={`
+        flex
+        flex-col
+        items-center
+        justify-center
+        gap-2
+        p-3
+        border-b-2
+        hover:text-neutral-800
+        transition
+        cursor-pointer
+        ${selected ? "border-b-neutral-800" : "border-transparent"}
+        ${selected ? "text-neutral-8000" : "text-neutral-500"}
+        `}
+    >
+      <Icon size={26} />
+      <div className="font-medium text-sm">{label}</div>
+    </div>
+  );
+};
+
+export default CategoryBox;
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,67 +9,64 @@ import bcrypt from "bcrypt";
 import prisma from "@/app/libs/prismadb";
 
 export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma),
 
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
 
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "email", type: "text" },
-        password: { label: "password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Invalid credentials");
-        }
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: { label: "email", type: "text" },
+                password: { label: "password", type: "password" },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error("Invalid credentials");
+                }
 
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email,
+                    },
+                });
 
-        if (!user || !user?.hashedPassword) {
-          throw new Error("Invalid credentials");
-        }
+                if (!user || !user?.hashedPassword) {
+                    throw new Error("Invalid credentials");
+                }
 
-        // compare and hash password of users
-        // const isCorrectPassword = await bcrypt.compare(
-        //   credentials.password,
-        //   user.hashedPassword
-        // );
-        const isCorrectPassword = await bcrypt.compare(
-          credentials.password,
-          user.hashedPassword
-        );
+                // compare and hash password of users
 
-        // if the password of user when entering doesn't match with the password was hashed in the database => throw error
+                const isCorrectPassword = await bcrypt.compare(
+                    credentials.password,
+                    user.hashedPassword
+                );
 
-        if (!isCorrectPassword) {
-          throw new Error("Invalid credentials");
-        }
+                // if the password of user when entering doesn't match with the password was hashed in the database => throw error
 
-        return user;
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/",
-  },
-  debug: process.env.NODE_ENV === "development",
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
+                if (!isCorrectPassword) {
+                    throw new Error("Invalid credentials");
+                }
+
+                return user;
+            },
+        }),
+    ],
+    pages: {
+        signIn: "/",
+    },
+    debug: process.env.NODE_ENV === "development",
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
 };
 
 export default NextAuth(authOptions);
