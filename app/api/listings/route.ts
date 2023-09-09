bbb191c79f2fc6ddced861b6bb1f5674e2e7baf1
Harry <danghuuphuc001@gmail.com>
2023-09-10 00:46:01 +0700

    direct server action & Listing card component

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
   }
 
   const body = await request.json();
-  console.log(body);
+  // console.log(body);
 
   // extract of the fields
   const {
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,54 @@
 // import Image from 'next/image'
 
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello airbnb</div>;
+import Container from "./components/Container";
+import ClientOnly from "./components/ClientOnly";
+import EmptyState from "./components/EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  const isEmpty = true;
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+        >
+          {/* render listing to UI with component that admin save in db */}
+          {listings.map((listing) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                data={listing}
+                currentUser={currentUser}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
